bd11f0c02903278ec20353015e25527930c44b85
Mike Rachman <mikerachman1@gmail.com>
2022-05-15 16:07:23 -0400

    Still having bugs with variables and logic of operating

--- script.js
+++ script.js
@@ -1,145 +1,82 @@
-const operate = (array) => {
-    switch(array[0]) {
-        case '+':
-            return Number(array[1]) + Number(array[2]);
-            break;
-        case '-':
-            return array[1] - array[2];
-            break;
-        case '*':
-            return array[1] * array[2];
-            break;
-        case '/':
-            return array[1] / array[2];
-            break;
-    }
+const operate = (n1, operator, n2) => {
+    const firstNum = parseFloat(n1)
+    const secondNum = parseFloat(n2)
+    if (operator === '+') return firstNum + secondNum
+    if (operator === '-') return firstNum - secondNum
+    if (operator === '*') return firstNum * secondNum
+    if (operator === '/') return firstNum / secondNum
 }
 
 let displayValue = 0;
+let firstNum = null;
+let secondNum = null;
+let operator = null;
 
-//num button declarations
 const display = document.querySelector('#display');
-const one = document.querySelector('#one');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
-const seven = document.querySelector('#seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
-const zero = document.querySelector('#zero');
-const decimal = document.querySelector('#decimal');
+//event listener for numbers
+const nums = document.querySelectorAll('.num');
+nums.forEach(num => {
+    num.addEventListener('click', () => {
+        if (displayValue === 0) {
+            display.textContent = num.textContent
+        } else {
+            display.textContent += num.textContent
+        }
+        displayValue = display.textContent
+    })
+})
+
+
 //operator button declarations
-const clear = document.querySelector('#clear')
 const addBtn = document.getElementById('add')
 const subtractBtn = document.getElementById('subtract')
 const multiplyBtn = document.getElementById('multiply')
 const divideBtn = document.getElementById('divide')
-const equalsBtn = document.getElementById('equals')
-//number button event listeners
-one.addEventListener('click', function() {
-    display.textContent += 1;
-    displayValue = display.textContent;
-})
-two.addEventListener('click', function() {
-    display.textContent += 2;
-    displayValue = display.textContent;
-})
-three.addEventListener('click', function() {
-    display.textContent += 3;
-    displayValue = display.textContent;
-})
-four.addEventListener('click', function() {
-    display.textContent += 4;
-    displayValue = display.textContent;
-})
-five.addEventListener('click', function() {
-    display.textContent += 5;
-    displayValue = display.textContent;
-})
-six.addEventListener('click', function() {
-    display.textContent += 6;
-    displayValue = display.textContent;
-})
-seven.addEventListener('click', function() {
-    display.textContent += 7;
-    displayValue = display.textContent;
-})
-eight.addEventListener('click', function() {
-    display.textContent += 8;
-    displayValue = display.textContent;
-})
-nine.addEventListener('click', function() {
-    display.textContent += 9;
-    displayValue = display.textContent;
-})
-zero.addEventListener('click', function() {
-    display.textContent += 0;
-    displayValue = display.textContent;
-})
+
+
+
+//decimal event listener
+const decimal = document.querySelector('#decimal');
 decimal.addEventListener('click', function() {
     display.textContent += '.';
     displayValue = display.textContent;
 })
 
 //clear button event listeners
+const clear = document.querySelector('#clear')
 clear.addEventListener('click', function() {
-    display.textContent = '';
+    display.textContent = '0';
     displayValue = 0;
+    firstNum = null;
+    secondNum = null;
+    operator = null;
 })
-//empty array for filling with numbers and operators 
-let arguments = [];
 
-
-//operator event functions
-const add = () => {
-    if (arguments.length === 2) {
-        arguments[2] = displayValue;
-        let results = operate(arguments);
-        displayValue = results;
-        display.textContent = results;
-        arguments = ['', results]
-    } else {
-        arguments[0] = '+';
-        arguments[1] = displayValue;
-        display.textContent = '';
-        displayValue = 0;
-    }
-};
-
-const subtract = () => {
-    arguments[0] = '-';
-    arguments[1] = displayValue;
-    display.textContent = '';
+const operators = document.querySelectorAll('.operator');
+operators.forEach(op => {
+    op.addEventListener('click', () => {
+        if (firstNum === null && secondNum === null) {
+            firstNum = display.textContent;
+            operator = op.textContent;
+            displayValue = 0;
+        } else if (firstNum !== null){
+            secondNum = display.textContent;
+            operator = op.textContent
+            let result = operate(firstNum, operator, secondNum);
+            display.textContent = result;
+            displayValue = 0;
+            firstNum = result;
+        }
+    })
+})
+//equals button event listener
+const equalsBtn = document.getElementById('equals')
+equalsBtn.addEventListener('click', () => {
+    secondNum = displayValue;
+    let result = operate(firstNum, operator, secondNum);
+    display.textContent = result;
     displayValue = 0;
-}
-
-const multiply = () => {
-    arguments[0] = '*';
-    arguments[1] = displayValue;
-    display.textContent = '';
-    displayValue = 0;
-}
-
-const divide = () => {
-    arguments[0] = '/';
-    arguments[1] = displayValue;
-    display.textContent = '';
-    displayValue = 0;
-}
-
-const equals = () => {
-    arguments[2] = displayValue;
-    let results = operate(arguments);
-    displayValue = results;
-    display.textContent = results;
-}
-//operator button event listeners
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply)
-divideBtn.addEventListener('click', divide)
-
-equalsBtn.addEventListener('click', equals)
+    firstNum = null;
+    secondNum = result;
+})
 
